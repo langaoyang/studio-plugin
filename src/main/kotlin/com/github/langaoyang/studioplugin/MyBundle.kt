6de5d495d56664fc8237702c6bca5f9d98eb9db3
GitHub Action <action@github.com>
2021-07-21 05:44:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/langaoyang/studioplugin/MyBundle.kt
+++ src/main/kotlin/com/github/langaoyang/studioplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.langaoyang.studioplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/langaoyang/studioplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/langaoyang/studioplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.langaoyang.studioplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.langaoyang.studioplugin.MyBundle
 
 class MyApplicationService {
 
